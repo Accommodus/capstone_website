3f2650405e96f698e49682355b4f693896958746
Cannon <cannonteaguewhitney8@gmail.com>
2023-01-05 21:50:36 -0600

    init commit

---
+++ content/machine_learning_tools.md
@@ -0,0 +1,6 @@
+Title: My First Review
+Date: 2010-12-03 10:20
+Thumbnail:
+Caption
+
+Following is a review of my favorite mechanical keyboard.
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: [
+      './output/*.html',
+    ],
+    media: false, // or 'media' or 'class'
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
