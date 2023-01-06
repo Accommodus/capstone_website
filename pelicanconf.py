14ba0c208c9061e658b498f8f3a30fe95ea0d196
Cannon <cannonteaguewhitney8@gmail.com>
2023-01-05 22:02:07 -0600

    2nd commit

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,6 +1,6 @@
 AUTHOR = 'Cannon Whitney'
 SITENAME = 'Machine Learning in your Life'
-SITEURL = ''
+SITEURL = 'lifeml.web.app'
 
 THEME = "theme"
 
@@ -16,16 +16,6 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
-
-# Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
 
